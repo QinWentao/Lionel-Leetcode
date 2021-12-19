d1f7e5364af27f399f87642f9171eb6468b3859e
QinWentao <799829099@qq.com>
2021-12-19 13:27:44 +0800

    两数之和

---
+++ Leetcode/Code/TwoNumbersSUM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Code
+{
+    public class TwoNumbersSUM
+    {
+        public static int[] nums = { 2, 0, 11, 15, 7 };
+        public static  int target = 9;
+        public static int[] Implementation_1() 
+        {
+            int n = nums.Length;
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            return new int[2] { i, j };
+                        }
+                    }
+                }
+            }
+            catch (Exception e) 
+            {
+            }
+            return new int[0];
+        }
+    }
+}
---
+++ Leetcode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Leetcode.Code;
+
+namespace Leetcode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public void TwoBumsSum() 
+        {
+            int[] result = TwoNumbersSUM.Implementation_1();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
